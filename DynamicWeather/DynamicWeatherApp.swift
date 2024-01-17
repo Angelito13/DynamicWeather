91616fe3c5bc29a62cb851dbb6d8990751a71b49
Angel Colon-Ramirez <angel.colon.ramirez@gmail.com>
2024-01-17 13:22:17 -0600

    Initial Commit

---
+++ DynamicWeather/DynamicWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicWeatherApp.swift
+//  DynamicWeather
+//
+//  Created by Angel Colon-Ramirez on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct DynamicWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
